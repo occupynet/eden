1bc34f01592546d9f63f6553780dde538c664cfd
Ross Glover <ross@ross.mayfirst.org>
2014-05-05 12:11:26 -0500

    Adding document ready to usi-swap.

--- js/usi-switch.js
+++ js/usi-switch.js
@@ -1,7 +1,10 @@
-var pathname = window.location.pathname;
-var usi = pathname.split('/');
-var logo = jQuery("#logo");
-if(usi[1] == 'usi') {
-  logo.attr("src","/mexcla/images/USi-logo.png");
-}
+$( document ).ready(function() {
+  var pathname = window.location.pathname;
+  var usi = pathname.split('/');
+  var logo = jQuery("#logo");
+  if(usi[1] == 'usi') {
+    logo.attr("src","/mexcla/images/USi-logo.png");
+  }
+});
+
 
